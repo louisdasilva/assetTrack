0566fec16301195fc3b30a483c88843bede030ca
louisdasilva <ldasilva@deakin.edu.au>
2023-12-23 21:30:28 +1000

    NEW: Add database connection.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+require('./dbConnection');
 
 // router
 const router = require('./routers/router')
