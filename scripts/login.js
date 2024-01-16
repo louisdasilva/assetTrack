46a25530c46114017cca967864cb9a0b023c8409
Dan Blair <s223215521@deakin.edu.au>
2024-01-16 21:53:03 +1100

    Added additional validation checks before sending to server. Added onscreen feedback message from server. Added max character lengths to username and password

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,5 @@
 const SERVER_URL = 'http://localhost:3000/';
+let loginFeedbackfield = document.getElementById("loginFeedback");    
 
 function checkForInvalidCharacters(userInput) {
     // DEFINITIONS
@@ -8,7 +9,7 @@ function checkForInvalidCharacters(userInput) {
     const SYMBOLS = ['!', '@', '#', '$', '%', '^', '&', '*', '_'];
     const VALID_CHARACTERS = ALPHABET.concat(NUMBERS, SYMBOLS);
     let match = false;
-    let invalidCharacter = false;
+    let isInvalidCharacter = false;
     // ITERATE THROUGH EACH CHARACTER AND COMPARE WITH VALID CHARACTERS
     userInput.split('').forEach(inputCharacter => {  
         match = false;  
@@ -16,76 +17,87 @@ function checkForInvalidCharacters(userInput) {
             if(inputCharacter == testCharacter) { match = true; }
         });
         if(!match) { 
-            invalidCharacter = true; 
+            isInvalidCharacter = true; 
         }  
     });
-    return invalidCharacter;
+    return isInvalidCharacter;
 }
 
 function validateInput() {
-    let formatPositive = "col-md-4 text-success d-flex align-items-center form-text";
+    let validUsername = false; 
+    let validPassword = false;
     let formatNegative = "col-md-4 text-danger d-flex align-items-center form-text";
 
     // VALIDATE USERNAME
-    const userName = $('#userName').val();
+    const userName = $('#userName').val(); // Get Username User Input
     let nameInputFeedbackText = document.getElementById("nameInputFeedbackText");
     if(userName == "") {
         nameInputFeedbackText.className = formatNegative;
         nameInputFeedbackText.innerHTML = "You did not enter a Username.";
+        loginFeedbackfield.innerHTML = "";
     }
     else if (checkForInvalidCharacters(userName)) {
         nameInputFeedbackText.className = formatNegative;
-        nameInputFeedbackText.innerHTML = "Username includes invalid character";
+        nameInputFeedbackText.innerHTML = "Invalid character in Username";
+        loginFeedbackfield.innerHTML = "";
     }
+    // Note Username max length (16 chars) is set in index.html
     else { 
-        nameInputFeedbackText.className = formatPositive;
-        nameInputFeedbackText.innerHTML = "Valid";
+        validUsername = true;
+        nameInputFeedbackText.innerHTML = "";
     }
 
     // VALIDATE PASSWORD SECTION
-    const userPassword = $('#userPassword').val();
+    const userPassword = $('#userPassword').val();  // Get Password User Input
     let passwordInputFeedbackText = document.getElementById("passwordInputFeedbackText");
 
     // VALIDATE PASSWORD CONDITIONS
     if(userPassword == "") {
         passwordInputFeedbackText.className = formatNegative;
         passwordInputFeedbackText.innerHTML = "You did not enter a Password.";
+        loginFeedbackfield.innerHTML = "";
     }
-    else if (userPassword.length < 8) {
+    else if (userPassword.length < 8) { // Note max length (64 chars) is set in index.html
         passwordInputFeedbackText.className = formatNegative;
         passwordInputFeedbackText.innerHTML = "Password must be greater than 7 characters.";
+        loginFeedbackfield.innerHTML = "";
     }
     else if (checkForInvalidCharacters(userPassword)) {
         passwordInputFeedbackText.className = formatNegative;
         passwordInputFeedbackText.innerHTML = "Invalid character in Password";
+        loginFeedbackfield.innerHTML = "";
     }
     else { 
-        passwordInputFeedbackText.className = formatPositive;
-        passwordInputFeedbackText.innerHTML = "Valid";
+        validPassword = true;
+        passwordInputFeedbackText.innerHTML = "";
     }
     //CALL CheckCredentials METHOD
-    if (passwordInputFeedbackText.innerHTML == "Valid" && nameInputFeedbackText.innerHTML == "Valid") {
+    if (validUsername && validPassword) {
         CheckCredentials(userName, userPassword); 
     }
 }
 
 function CheckCredentials(userName, userPassword) {
-
     let url = SERVER_URL + 'userValidate/' + userName;
+    let formatNegative = "text-danger text center form-text";
+
     
     $.get(url) //Here userName is used to GET the matching object with the same userName from the database.
         .then(response => {
             if (response.data == undefined) { //First we must check if a matching object was found.
-                alert("Error! \n Username not found, please contact admin.");
-                location.href=SERVER_URL;
+                alert("Username not found, try \n Username: admin \n Password: assetTrack1! \nAlert to be removed.");
+                loginFeedbackfield.className = formatNegative;
+                // Provide feedback - but don't let user know if username is correct for security.
+                loginFeedbackfield.innerHTML = "Username or Password does not match. If issue persists, please contact administrator for support.";
             }
             //NOW CHECK PASSWORD - Note checking for username is redundant as we already navigated to object with userName.
             else if (userPassword === response.data.password) {
                 location.href = SERVER_URL + 'parts.html';
             }
             else {
-                alert("Error! \n That password does not match the username.");
-                location.href=SERVER_URL;
+                alert("Password does not match, try \n Username: admin \n Password: assetTrack1! \nAlert to be removed.");
+                loginFeedbackfield.className = formatNegative;
+                loginFeedbackfield.innerHTML = "Username or Password does not match. If issue persists, please contact administrator for support.";
             }
         });
 }
