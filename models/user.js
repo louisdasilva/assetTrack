987a9d9ba4faabfb38dfb40c85ff5f7c37f57243
louisdasilva <ldasilva@deakin.edu.au>
2024-01-20 21:57:09 +1000

    Document: Add comments with references

--- models/user.js
+++ models/user.js
@@ -1,12 +1,22 @@
 let client = require('../dbConnection');
 let collection = client.db().collection('Users');
 
-
+/*
+ * Search user collection for one document with matching username and password.
+ * Uses projection to only return username if a document is found.
+ * If no matching document is found the return will be null.
+ * 
+ * documentation regarding searches @
+ * https://www.mongodb.com/docs/manual/tutorial/query-documents/
+ * 
+ * documentation regarding projection @
+ * https://www.mongodb.com/docs/manual/tutorial/project-fields-from-query-results/
+ */
 async function getUser(userName, userPassword) {
     try {
         const userData = await collection.findOne(
             {username:userName,password:userPassword},
-            {projection: {username:1} } // https://www.mongodb.com/docs/manual/tutorial/project-fields-from-query-results/
+            {projection: {username:1} } 
         );
         return userData;
     } catch (error) {
