efc75c6f4a43934aec5aa4964f6a8ba73acf1ec0
louisdasilva <ldasilva@deakin.edu.au>
2024-01-29 08:36:29 +1000

    NEW: Append footer to each page from index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://localhost:3000'; 
+const SERVER_URL = 'http://localhost:3000';
 
 function getSession() {
 	// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
@@ -8,15 +8,18 @@ function getSession() {
 
 function cataloguePage() {
 	const session = getSession();
-	location.href = SERVER_URL + '/catalogue?sk=' + session; // request page at server endpoint that matches INPUT SERVER ENDPOINT HERE!
+	location.href = SERVER_URL + '/catalogue?sk=' + session;
 }
 
 function dashboardPage() {
 	const session = getSession();
-	location.href = SERVER_URL + '/dash?sk=' + session; // request page at server endpoint that matches INPUT SERVER ENDPOINT HERE!
+	location.href = SERVER_URL + '/dash?sk=' + session;
 }
 
 function inventoryPage() {
 	const session = getSession();
-	location.href = SERVER_URL + '/inventory?sk=' + session; // request page at server endpoint that matches INPUT SERVER ENDPOINT HERE!
+	location.href = SERVER_URL + '/inventory?sk=' + session;
 }
+
+// Appends a Footer to every webpage
+$('.footer').append("Copyright: AssetTrack 2024");
