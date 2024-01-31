24577db72c576bf3ea589c7076efefae47a563c2
louisdasilva <ldasilva@deakin.edu.au>
2024-01-31 21:22:03 +1000

    MOD: Take navbar out of each HTML page and have index.js auto-add nav buttons to each page as appropriate for that page

--- scripts/index.js
+++ scripts/index.js
@@ -21,5 +21,22 @@ function inventoryPage() {
 	location.href = SERVER_URL + '/inventory?sk=' + session;
 }
 
+function opsInventoryPage() {
+	const session = getSession();
+	location.href = SERVER_URL + '/opsInventory?sk=' + session;
+}
+
+// Append a nav-bar to every page based on which page is the currently viewed page
+const page = document.title;
+if(page != "assetTrack - Dashboard"){
+	$('#pageNavbar').append('<button type="button" class="btn btn-primary me-1 dataButton" id="navButton_to_dashboard" onclick="dashboardPage()">Home</button>');
+}
+if(page != 'assetTrack - Catalogue'){
+	$('#pageNavbar').append('<button type="button" class="btn btn-primary me-1 dataButton" id="navButton_to_catalogue" onclick="cataloguePage()">Parts Catalogue</button>');
+}
+if(page != 'assetTrack - Inventory'){
+	$('#pageNavbar').append('<button type="button" class="btn btn-primary me-1 dataButton" id="navButton_to_inventory" onclick="inventoryPage()">Inventory</button>');
+}
+
 // Appends a Footer to every webpage
 $('.footer').append("Copyright: AssetTrack 2024");
